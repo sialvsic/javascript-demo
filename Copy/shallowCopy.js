e0380694a1f78d607e74cfea0ec7479077f0961d
zhou.yixing <zhou.yixing@rea-group.com>
2019-02-26 22:22:40 +0800

    Add copy

--- Copy/shallowCopy.js
+++ Copy/shallowCopy.js
@@ -1,10 +1,18 @@
-function shallowCopy(target, sources) {
-
+function shallowCopy(src) {
+  let dst = {};
+  for(let prop in src) {
+    if(src.hasOwnProperty(prop)) {
+      dst[prop] = src[prop];
+    }
+  }
+  return dst;
 }
 
-shallowCopy({}, {
+let copy = shallowCopy({
   name: '123',
   test: {
     good: true,
   }
 });
+
+console.log(copy);
