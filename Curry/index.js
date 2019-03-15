90f84e111bdaf4726b37d71025f063201178e786
sialvsic <sialvsic@outlook.com>
2019-03-15 13:21:49 +0800

    test fetch

--- Curry/index.js
+++ Curry/index.js
@@ -1,34 +1,74 @@
-function curry(func) {
-  var fixedArgs = [].slice.call(arguments, 1);
-  return function() {
-    let args = fixedArgs.concat([].slice.call(arguments));
-    return func.apply(null, args);
+/*
+add(1)(2)(3) = 6
+add(1, 2, 3)(4) = 10
+add(1)(2)(3)(4)(5) = 15
+*/
+
+function _add(a) {
+  return function(b) {
+    return a + b;
+  }
+}
+
+function add() {
+  let _args = [].slice.call(arguments);
+  let adder = function() {
+
+    let _adder = function() {
+      [].push.apply(_args, [].slice.call(arguments));
+      return _adder;
+    };
+
+    _adder.toString = function() {
+      return _args.reduce(function(a, b) {
+        return a + b;
+      });
+    };
+
+    return _adder;
   };
+
+  return adder.apply(null, [].slice.call(arguments))
 }
 
 
-function currying(fn) {
-  let allArgs = [];
+function add1(){
+  let args = [].slice.call(arguments);
 
-  return function next() {
-    let args = [].slice.call(arguments);
+  function name() {
+    let _add = () => {
 
-    if(args.length > 0) {
-      allArgs = allArgs.concat(args);
-      return next;
-    } else {
-      return fn.apply(null, allArgs);
-    }
+    };
+
+    _add.toString()
+
+    return _add;
   }
+
+  return name.apply(null, [].slice.call(arguments))
 }
 
-var add = currying(function() {
-  var sum = 0;
-  for (var i = 0; i < arguments.length; i++) {
-    sum += arguments[i];
+// 输出结果，可自由组合的参数
+// console.log(add(1, 2, 3, 4, 5));  // 15
+// console.log(add(1, 2, 3, 4)(5));  // 15
+
+console.log(add(1)(2)(3)(4)(5));  // 15
+
+var currying = function(fn) {
+  let args = [].slice.call(arguments, 1);
+
+  return function() {
+    let _args = args.concat([].slice.call(arguments))
+    return fn.apply(null, _args)
   }
-  return sum;
+};
+
+let sum = currying(function() {
+  let args = [].slice.call(arguments);
+  return args.reduce(function(a, b) {
+    return a + b;
+  })
 });
 
-
-add(1)(2, 3)(4)()
+console.log(sum(1, 2, 3, 4, 5));  // 15
+console.log(sum(1)(2)(3)(4)(5));  // 15
