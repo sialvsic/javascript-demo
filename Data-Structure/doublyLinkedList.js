d52b8589f7d6acb1aedbd2f30924f20598c489fe
sialvsic <sialvsic@outlook.com>
2019-04-08 22:28:05 +0800

    finish doublyLinkedList

--- Data-Structure/doublyLinkedList.js
+++ Data-Structure/doublyLinkedList.js
@@ -5,8 +5,8 @@ function Node(element) {
 }
 
 function DoublyLinkedList() {
-  let head = null;
-  let tail = null;
+  this.head = null;
+  this.tail = null;
   this.length = 0;
 }
 
@@ -38,7 +38,7 @@ DoublyLinkedList.prototype.insert = function(position, element) {
         current.previous = node;
         this.head = node;
       }
-    } else if(position === length) {
+    } else if(position === this.length) {
       node.previous = this.tail;
       this.tail.next = node;
       this.tail = node;
@@ -56,3 +56,99 @@ DoublyLinkedList.prototype.insert = function(position, element) {
     throw new Error('position is not valid');
   }
 };
+
+DoublyLinkedList.prototype.removeAt = function(position) {
+  if(position >= 0 && position <= this.length) {
+    let index = 0;
+    let previous;
+    let current = this.head;
+
+    if(position === 0) {
+
+      if(this.head === null) {
+        return null;
+      } else if(this.head.next === null) {
+        this.head = null;
+        this.tail = null;
+      } else {
+        this.head = this.head.next;
+        this.head.previous = null;
+      }
+    } else if(position === length) {
+      this.tail = this.tail.previous;
+      this.tail.next = null;
+    } else {
+      while (index++ < position) {
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+      current.next.previous = previous;
+
+    }
+    this.length--;
+    return current.element;
+  } else {
+    throw new Error('position is not valid');
+  }
+};
+
+DoublyLinkedList.prototype.indexOf = function(element) {
+  let current = this.head;
+  let index = -1;
+  if(element === current.element) {
+    return 0;
+  }
+  index++;
+  while (current.next !== null) {
+    if(element === current.element) {
+      return index;
+    }
+    current = current.next;
+    index++;
+  }
+
+  if(element === current.element) {
+    return index;
+  }
+
+  return -1;
+};
+
+DoublyLinkedList.prototype.remove = function(element) {
+  const index = this.indexOf(element);
+  return this.removeAt(index);
+};
+
+DoublyLinkedList.prototype.isEmpty = function() {
+  return this.length === 0;
+};
+
+DoublyLinkedList.prototype.size = function() {
+  return this.length;
+};
+
+DoublyLinkedList.prototype.getHead = function() {
+  return this.head;
+};
+
+DoublyLinkedList.prototype.getTail = function() {
+  return this.tail;
+};
+
+DoublyLinkedList.prototype.clear = function() {
+  this.length = 0;
+  this.head = null;
+  this.tail = null;
+};
+
+const doublyLinkedList = new DoublyLinkedList();
+doublyLinkedList.add(1);
+doublyLinkedList.add(2);
+doublyLinkedList.add(3);
+doublyLinkedList.add(4);
+doublyLinkedList.add(5);
+doublyLinkedList.add(6);
+doublyLinkedList.insert(2, 200);
+
+console.log(doublyLinkedList);
