a8d048e49813421b08b10545a4f3e66b5588e2ca
sialvsic <sialvsic@outlook.com>
2019-07-01 23:22:13 +0800

    add complex curry

--- FP/Curry/currying.js
+++ FP/Curry/currying.js
@@ -1,19 +1,40 @@
-//currying 通用式
-var currying = function(fn) {
-  var args = [].slice.call(arguments, 1);
+//currying 基本通用式
+let currying = function (fn) {
+  let args = [].slice.call(arguments, 1);
 
-  return function() {
+  return function () {
     // 主要还是收集所有需要的参数到一个数组中，便于统一计算
-    var _args = args.concat([].slice.call(arguments));
+    let _args = args.concat([].slice.call(arguments));
     return fn.apply(null, _args);
-  }
+  };
 };
 
-var sum = currying(function() {
-  var args = [].slice.call(arguments);
-  return args.reduce(function(a, b) {
+let sum = currying(function () {
+  let args = [].slice.call(arguments);
+  return args.reduce(function (a, b) {
     return a + b;
-  })
+  });
 }, 10);
 
-console.log(sum(10, 2));
+console.log(sum(10, 2, 5));
+
+//currying 复杂通用式
+let curry = function (fn, len) {
+
+  const length = len || fn.length;
+
+  return function (...rest) {
+    console.log(rest);
+    return rest.length >= length ?
+      fn.apply(this, rest) :
+      curry(currying.apply(this, [fn].concat(rest), length - rest.length));
+  };
+};
+
+function sayHello(name, age, fruit) {
+  console.log(`我叫${ name }, 我${ age }岁了, 我喜欢吃${ fruit }`);
+}
+
+const betterShowMsg = curry(sayHello);
+// betterShowMsg('小A', 20, '西瓜');
+betterShowMsg('小A')(20, '西瓜');
