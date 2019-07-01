a587a5c586942c996f2d3d99119e051028a7a5b5
sialvsic <sialvsic@outlook.com>
2019-07-01 22:44:21 +0800

    add inherit demo

--- Inherit/prototype.js
+++ Inherit/prototype.js
@@ -1,13 +1,21 @@
-//原型链继承
-
-class A {
-  constructor(name){
-    this.name = name
-  }
+function Animal(special) {
+  this.special = special;
 }
 
-class B extends A{
-  constructor(){
-    super();
-  }
+Animal.prototype.getAge = function () {
+  return this.age;
+};
+
+function Cat() {
 }
+
+Cat.prototype = new Animal();
+Cat.prototype.constructor = Cat; // 将Cat.prototype.constructor重新指向本身
+
+/**
+ * 测试代码
+ */
+let cat = new Cat();
+cat.getAge(); // output: Animal
+console.log(cat.prototype);
+console.log(cat.species); // undefined
