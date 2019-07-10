53db90ee3a1eb8567bf89de9d908a89b6665ad5c
sialvsic <sialvsic@outlook.com>
2019-07-10 19:47:43 +0800

    add details in inheit

--- Inherit/prototype.js
+++ Inherit/prototype.js
@@ -6,16 +6,20 @@ Animal.prototype.getAge = function () {
   return this.age;
 };
 
-function Cat() {
+function Cat(name) {
+  this.name = name;
 }
 
 Cat.prototype = new Animal();
 Cat.prototype.constructor = Cat; // 将Cat.prototype.constructor重新指向本身
+Cat.prototype.getName = function () {
+  return this.name;
+};
 
 /**
  * 测试代码
  */
-let cat = new Cat();
+let cat = new Cat('cat');
 cat.getAge(); // output: Animal
-console.log(cat.prototype);
+console.log(cat.getName()); // output: Animal
 console.log(cat.species); // undefined
