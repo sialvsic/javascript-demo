079c34462ec629e6082f3f3db1f32e35c4da9c1e
sialvsic <sialvsic@outlook.com>
2019-07-26 21:22:31 +0800

    remove the wrong new function

--- This/new.js
+++ This/new.js
@@ -17,12 +17,4 @@ var a = _new(People);
 var b = _new();
 
 
-function new__(fn, ...args) {
-  let obj = {};
-  Object.setPrototypeOf(obj, fn.prototype);
-  let result = fn();
-
-  return result instanceof Object ? result : obj;
-}
-
 
