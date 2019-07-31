88b98b03dd5f85d7222f354f94b203470c4a6d0e
sialvsic <sialvsic@outlook.com>
2019-07-31 14:31:49 +0800

    add Iterator implement

---
+++ ES2015/Iterator.js
@@ -0,0 +1,31 @@
+//添加一个Iterator接口
+const obj = {
+  name: 'this is a object without iterator'
+};
+
+// for (let value of obj) {
+//  console.log(value)
+//  TypeError: obj is not iterable
+// }
+
+const obj_iterator = {
+  name: 'this is a object without iterator',
+  [Symbol.iterator]() {
+    let index = 0;
+    return {
+      next: () => {
+        let keys = Object.keys(this);
+        return index < keys.length ? {
+          done: false,
+          value: this[keys[index++]]
+        } : {
+          done: true
+        }
+      }
+    }
+  }
+};
+
+for (let v of obj_iterator) {
+  console.log(v)
+}
